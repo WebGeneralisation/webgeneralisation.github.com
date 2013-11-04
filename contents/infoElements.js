933b63b4f51023baecddf60229a984a78755d0d2
milkbread <klammer@klammer-ubuntu.(none)>
2013-11-04 16:14:08 +0100

    working on contents

--- contents/infoElements.js
+++ contents/infoElements.js
@@ -4,23 +4,51 @@
 //**********
 //elements for ... introduction.html
 var introInfo = [
-  {'head':'play', 'ref':'https://github.com/WebGeneralisation', 'content':'...a playground', 
+  {	'head':'play',
+  	'ref':'', 
+  	'content':'a playground', 
   	'description':[
-  		"...it doesn't matter if you do something clever...strange...cool...necessary...impressive...",
-  		'- WebGen should be the place for everything -'
-  	]},
-  {'head':'know_base', 'ref':'https://github.com/WebGeneralisation/webgeneralisation.github.com/wiki', 'content':'...a Knowledge-Base', 
+  		"...it doesn't matter if you do something clever...strange...cool...necessary...impressive..."  		
+  	],
+  	"catch":'WebGen should be the place for everything'},
+  {	'head':'know_base',
+  	'ref':'https://github.com/WebGeneralisation/webgeneralisation.github.com/wiki', 
+  	'content':'a Knowledge-Base', 
   	'image':'http://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Wikipedia-logo-en-big.png/196px-Wikipedia-logo-en-big.png',
   	'description':[
-  		"...Wikipedia has led the way ... we should learn from that...",
-  		"- Let's build tools that help collecting knowledge about generalisation -"]},
-  {'head':'plato', 'ref':'', 'content':'...a Platform', 'description':[
-  	"...generalisation can be difficult to understand and has partially philosophical aspects...",
-  	"- WebGen should provide different areas for Developers, Users and Acadamics -"]},
-  {'head':'net', 'ref':'', 'content':'...a Network and Community', 'description':''},
-  {'head':'open', 'ref':'', 'content':'...an Open Project', 'description':'', 
-		  	'image':'https://github-media-downloads.s3.amazonaws.com/github-logo.svg'},
-  {'head':'old', 'ref':'', 'content':"...an 'old' Idea" , 'description':''}
+  		"...Wikipedia has led the way ... we should learn from that..."
+  	],
+  	"catch":"Let's build tools that help collecting knowledge about generalisation"},
+  {	'head':'plato', 
+  	'ref':'', 
+  	'content':'a Platform', 
+  	'description':[
+	  	"...generalisation can be difficult to understand and has partially philosophical aspects...",
+	  	"...generalisation is necessary for a wide range of people: Developers, Users, Acadamics, NMAs, ..."
+	],
+  	"catch": "WebGen should be indisensible for all aspects of Generalisation"},
+  {	'head':'net', 
+  	'ref':'', 
+  	'content':'a Network and Community', 
+  	'description':[
+  	"...'Web 2.0' is not just a catchword - it involves all the necessary aspects of modern communication...",
+  	"...Twitter, Mailinglists, etc. - WebGen uses these tools to benefit from their indisputable advantages..."
+  	],
+  	"catch": "Neogeneralisation is the catchword that describes WebGen"},
+  {	'head':'open', 
+  	'ref':'https://github.com/WebGeneralisation', 
+  	'content':'an Open Project', 
+  	'description':[
+  	"...everything, that we do is visible to everybody...",
+  	"...GitHub provides the perfect framework for collaborative development of algorithms, tools, projects, etc..."
+  	],
+  	"catch": "GitHub is the central point of relevance for all developments", 
+	'image':'https://github-media-downloads.s3.amazonaws.com/github-logo.svg'},
+  {	'head':'old', 
+  	'ref':'', 
+  	'content':"an 'old' Idea" , 
+  	'description':[],
+  	"catch": ""}
 
 ];
 
@@ -42,9 +70,9 @@ function showInfos(container, active){
 	    .attr('id',function(d){return d.head});
 	//heading
 	contentContainers.append('p')
-	      .attr('class','t-align-c f36 sansserif dark_grey')
+	      .attr('class','t-align-c f36 sansserif dark_grey margin_t0')
 	      .attr('href',function(d){return d.ref})
-	      .text(function(d){return d.content})
+	      .text(function(d){return "... " + d.content})
 
 	//add the logo at the rigth place
 	contentContainers.append('div').attr('class','margin3').attr('id',function(d){if(d.head=='play')return 'logo'; else return 'nothing'})
@@ -54,12 +82,15 @@ function showInfos(container, active){
 	      .attr('href',function(d){return d.ref})
 	      .append('img')
 		      .attr('src',function(d){return d.image})
-		      .attr('class','i-align-c margin_b2')	
+		      .attr('class','i-align-c margin_b2 margin_t0')	
 	//explanations	      
 	contentContainers.selectAll('#explanation').data(function(d){return d.description}).enter()
 		.append('p').attr('id','explanation')
 	      .attr('class','t-align-c f16 margin_b2 sansserif dark_grey')
 	      .text(function(d){return d})
+	//explanations	      
+	contentContainers.append('p').attr('class','t-align-c f22 margin_b0 sansserif dark_grey2')
+	      .text(function(d){return "- " + d.catch + " -"})
 
 	var logo = d3.select('#logo');
   	var logo_ = new webgen_logo(logo, window.innerWidth/2, false);
